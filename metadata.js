1addeb565757e88d9d4e40c1427408f23bd8a57f
Jelmer Oosthoek <jhpoosthoek@gmail.com>
2013-07-12 14:33:09 +0200

    Fixed error in metadata.js

--- metadata.js
+++ metadata.js
@@ -1,15 +1,17 @@
 function initmetadata()
     {
-    var descov = getBinary(planetserver_wcsdc + hsdataset.collection);
-    descov = descov.match(/gmlcov:metadata>(.+)<\/gmlcov:metadata/)[0];
-    descov = descov.replace('minimum','"minimum"');
-    descov = descov.replace('maximum','"maximum"');
-    descov = descov.replace('mean','"mean"');
-    descov = descov.replace('stddev','"stddev"');
-    descov = descov.replace('wavelength','"wavelength"');
-    descov = descov.replace('fwhm','"fwhm"');
-    descov = descov.replace('bbl','"bbl"');
-    descov = '' + descov.substring(16,descov.length - 18);
+    var descov = getBinary('metadata/' + hsdataset.collection + '.js');
+    descov = descov.replace(';','');
+    // var descov = getBinary(planetserver_wcsdc + hsdataset.collection);
+    // descov = descov.match(/gmlcov:metadata>(.+)<\/gmlcov:metadata/)[0];
+    // descov = descov.replace('minimum','"minimum"');
+    // descov = descov.replace('maximum','"maximum"');
+    // descov = descov.replace('mean','"mean"');
+    // descov = descov.replace('stddev','"stddev"');
+    // descov = descov.replace('wavelength','"wavelength"');
+    // descov = descov.replace('fwhm','"fwhm"');
+    // descov = descov.replace('bbl','"bbl"');
+    // descov = '' + descov.substring(16,descov.length - 18);
     hsdataset.metadata = JSON.parse(descov);
     
     data = JSON.parse(getBinary('wcps.php?use=metadata&collection=' + hsdataset.collection));
